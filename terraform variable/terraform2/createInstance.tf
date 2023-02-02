8df815f1c883b782e3da8a5f0c30adbcf24235c8
KETAN THOMBARE <ketan.thombare@gmail.com>
2023-02-03 01:17:49 +0530

    ghkj

--- terraform variable/terraform2/createInstance.tf
+++ terraform variable/terraform2/createInstance.tf
@@ -1,5 +1,15 @@
 data "aws_availability_zones" "available" {}
 
+data "aws_ami" "latest_ubuntu" {
+  most_recent = true
+  owner = ["099720109477"]
+
+  filter {
+    name = "name"
+    values = "ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-sever-*"
+  }
+}
+
 
 resource "aws_instance" "MyFirstInstnace" {
   ami           = lookup(var.AMIS, var.AWS_REGION)
