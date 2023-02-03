f011dc1a2cbf2abfc67cc7c71eb453a2575ba735
KETAN THOMBARE <ketan.thombare@gmail.com>
2023-02-03 09:24:03 +0530

    added update

--- terraform variable/terraform2/createInstance.tf
+++ terraform variable/terraform2/createInstance.tf
@@ -2,11 +2,16 @@ data "aws_availability_zones" "available" {}
 
 data "aws_ami" "latest_ubuntu" {
   most_recent = true
-  owner = ["099720109477"]
+  owner = ["099720109477 "] 
 
   filter {
     name = "name"
     values = "ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-sever-*"
+  }
+  
+  filter {
+    name = "virtulization-type"
+    values = ["hvm"]
   }
 }
 
